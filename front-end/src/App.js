7829b7080057d9eb4b11dc9f18dda0eb09e50b44
donncha <chdonncha@gmail.com>
2022-07-25 01:11:21 +0100

    basic routing setup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import {BrowserRouter as Router, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ErrorPage from "./pages/ErrorPage";
+import KanaQuiz from "./pages/KanaQuiz";
+import Home from "./pages/Home";
 
 function App() {
-    return <Router>
-        <a href="/home"> Go to home page</a>
-        <Routes></Routes></Router>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/kanaquiz" element={<KanaQuiz />}/>
+                <Route path="*" element={<ErrorPage />}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ front-end/src/pages/KanaQuiz.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function KanaQuiz() {
+    return <div>THIS IS THE KANAQUIZ PAGE</div>
+}
+
+export default KanaQuiz;
