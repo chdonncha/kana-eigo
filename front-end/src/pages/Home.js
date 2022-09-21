33d75a8a91d8dcf0827277d4ebe42dc5ea183370
chdonncha <chdonncha@gmail.com>
2022-09-21 23:41:45 +0100

    navbar updates

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -9,11 +9,11 @@ function Home() {
         <>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="/">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/kanaquiz">Kana Quiz</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
