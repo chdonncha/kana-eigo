684e406a7c8ee31dc4b144f4b22374407a6a0fa9
chdonncha <chdonncha@gmail.com>
2022-09-22 23:39:12 +0100

    Removed redundant code

--- front-end/src/components/NavbarComponent.js
+++ front-end/src/components/NavbarComponent.js
@@ -18,17 +18,4 @@ const NavbarComponent = () => {
     )
 }
 
-// <>
-//     <Navbar bg="dark" variant="dark">
-//         <Container>
-//             <Navbar.Brand href="/">Navbar</Navbar.Brand>
-//             <Nav className="me-auto">
-//                 <Nav.Link href="/">Home</Nav.Link>
-//                 <Nav.Link href="/kanaquiz">Kana Quiz</Nav.Link>
-//                 <Nav.Link href="/about">About</Nav.Link>
-//             </Nav>
-//         </Container>
-//     </Navbar>
-// </>
-
 export default NavbarComponent;
