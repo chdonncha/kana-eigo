55c278bb6f7bd76a159b48907a83c9f66ad5467c
chdonncha <chdonncha@gmail.com>
2022-09-22 23:38:01 +0100

    Created navbar component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,16 +3,21 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ErrorPage from "./pages/ErrorPage";
 import KanaQuiz from "./pages/KanaQuiz";
 import Home from "./pages/Home";
+import NavbarComponent from "./components/NavbarComponent";
+import React from "react";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/kanaquiz" element={<KanaQuiz />}/>
-                <Route path="*" element={<ErrorPage />}/>
-            </Routes>
-        </Router>
+        <>
+            <Router>
+                <NavbarComponent />
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/kanaquiz" element={<KanaQuiz/>}/>
+                    <Route path="*" element={<ErrorPage/>}/>
+                </Routes>
+            </Router>
+        </>
     );
 }
 
