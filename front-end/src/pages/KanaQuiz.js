f7f54cb79ae818a89dfd0cd3c9f57a225ab90fb9
chdonncha <chdonncha@gmail.com>
2022-09-26 00:34:01 +0100

    Styling katanquiz page

--- front-end/src/pages/KanaQuiz.js
+++ front-end/src/pages/KanaQuiz.js
@@ -4,12 +4,28 @@ import {Col, Row} from "react-bootstrap";
 
 function KanaQuiz() {
     return (
-        <Container fluid="md" className="text-center">
+        <Container fluid="md" className="text-center mt-5">
             <Row>
                 <Col>
                     <h3>Translate Katakana to English</h3>
                 </Col>
             </Row>
+            <Row>
+                <Col>
+                    <h3 className="mt-5">マクドナルド</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <input className="p-2 bg-light border mt-3"></input>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <button className="mt-3">Submit Answer</button>
+                </Col>
+            </Row>
+            <div className='mb-5'></div>
         </Container>
     )
 }
