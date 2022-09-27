40c66456532b0bda5b282edda58417a8388a380d
chdonncha <chdonncha@gmail.com>
2022-09-27 22:25:34 +0100

    Display Json in list

--- front-end/src/pages/KanaQuiz.js
+++ front-end/src/pages/KanaQuiz.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Container from "react-bootstrap/Container";
 import {Col, Row} from "react-bootstrap";
 
 function KanaQuiz() {
+    var question = "マクドナルド";
+
+    const [data,setData]=useState([]);
+    const getData=()=>{
+        fetch('KanaEngData.json'
+            ,{
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then(function(response){
+                console.log(response)
+                return response.json();
+            })
+            .then(function(myJson) {
+                console.log(myJson);
+                setData(myJson)
+            });
+    }
+    useEffect(()=>{
+        getData()
+    },[]);
+
+    console.log(data);
+    //
+    // var randomProperty = function (data) {
+    //     console.log(data);
+    //     var keys = Object.keys(data);
+    //     return data[keys[keys.length * Math.random() << 0]];
+    // };
+
+    const dataItemToKeyValues = (data) => {
+        const entries = Object.entries(data);
+        const listItems = entries.map(([key, value]) => (
+            <li>
+                {key}: {value}
+            </li>
+        ));
+        return <ul>{listItems}</ul>;
+    };
+
     return (
         <Container fluid="md" className="text-center mt-5">
             <Row>
@@ -12,7 +55,13 @@ function KanaQuiz() {
             </Row>
             <Row>
                 <Col>
-                    <h3 className="mt-5">マクドナルド</h3>
+                    <h3 className="mt-5">
+                        {
+                            data.map((item) => (
+                                    <li>{dataItemToKeyValues(item)}</li>
+                                ))
+                        }
+                    </h3>
                 </Col>
             </Row>
             <Row>
@@ -29,5 +78,6 @@ function KanaQuiz() {
         </Container>
     )
 }
+
 
 export default KanaQuiz;
